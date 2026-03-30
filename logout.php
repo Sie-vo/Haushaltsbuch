9c6780cbaa786a7de1cf377455a6f1642bfc2278
Sievo <sievo@noreply.codeberg.org>
2026-03-30 14:28:20 +0200

    Dateien nach „/“ hochladen

---
+++ logout.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Author: Dennis Vorpahl
+Copyright (c) 2026 Dennis Vorpahl
+https://sievo.de
+info@sievo.de
+
+logout.php Version 1.0.0 
+
+Diese Datei ist Teil von Sievo Haushaltsbuch.
+
+    Sievo Haushaltsbuch ist Freie Software: Sie können es unter den Bedingungen
+    der GNU General Public License, wie von der Free Software Foundation,
+    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+    veröffentlichten Version, weiter verteilen und/oder modifizieren.
+
+    Sievo Haushaltsbuch wird in der Hoffnung, dass es nützlich sein wird, aber
+    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+    Siehe die GNU General Public License für weitere Details.
+
+    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+    Programm erhalten haben. Wenn nicht, siehe <https://www.gnu.org/licenses/>.*/
+
+require_once "auth.php";
+session_destroy();  
+header("Location: login.php");
